95291c133d8b646334121f3fbf8282182167341a
rain-knows <justlikezyp@gmail.com>
2026-04-22 23:37:05 +0800

    refactor: 删除原套件

--- frontend/src/features/editor/components/index.ts
+++ frontend/src/features/editor/components/index.ts
@@ -1,6 +1,1 @@
-export { EditorRoot } from '@/features/editor/components/EditorRoot';
-export { EditorContent } from '@/features/editor/components/EditorContent';
-export { CommandMenu } from '@/features/editor/components/CommandMenu';
-export { BubbleMenu } from '@/features/editor/components/BubbleMenu';
-export { EditorShell } from '@/features/editor/components/EditorShell';
 export { EditorEntryTemplate } from '@/features/editor/components/EditorEntryTemplate';
--- frontend/src/features/editor/index.ts
+++
@@ -1,3 +0,0 @@
-export * from '@/features/editor/types';
-export * from '@/features/editor/components';
-export * from '@/features/editor/utils/image-upload-handlers';
